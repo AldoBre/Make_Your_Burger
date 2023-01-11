10d7143a32dfc8cd47a5d32fd42a9bedb15c846c
Aldo Bressan <aldobresssan@hotmail.com>
2023-01-11 17:23:13 -0300

    First Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Banner />
+        <div class="main-container">
+            <h1>Monter o seu burguer: </h1>
+            <BurguerForm />
+        </div>
+    </div>
+</template>
+
+<script>
+import Banner from "@/components/Banner.vue"
+import BurguerForm from "@/components/BurguerForm.vue";
+
+export default{
+    name:'Home',
+    components:{
+        Banner,
+        BurguerForm
+    }
+}
+
+</script>
